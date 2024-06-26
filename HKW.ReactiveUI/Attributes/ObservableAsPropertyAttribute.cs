3862c081a6b237fb70a920b6ec2daaeb9ec7fc66
Hakoyu <Hakoyu@outlook.com>
2024-06-26 23:01:01 +0800

    更新

--- HKW.ReactiveUI/Attributes/ObservableAsPropertyAttribute.cs
+++ HKW.ReactiveUI/Attributes/ObservableAsPropertyAttribute.cs
@@ -1,12 +1,12 @@
-﻿// Copyright (c) 2023 .NET Foundation and Contributors. All rights reserved.
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for full license information.
+﻿//// Copyright (c) 2023 .NET Foundation and Contributors. All rights reserved.
+//// Licensed to the .NET Foundation under one or more agreements.
+//// The .NET Foundation licenses this file to you under the MIT license.
+//// See the LICENSE file in the project root for full license information.
 
-namespace HKW.HKWReactiveUI;
+//namespace HKW.HKWReactiveUI;
 
-/// <summary>
-/// Attribute that marks an ObservableAsPropertyHelper for weaving.
-/// </summary>
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
-public sealed class ObservableAsPropertyAttribute : Attribute { }
+///// <summary>
+///// Attribute that marks an ObservableAsPropertyHelper for weaving.
+///// </summary>
+//[AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+//public sealed class ObservableAsPropertyAttribute : Attribute { }
