ae9ac8b4fc14c4c63cff460b8c1b0d36b3081e05
Hakoyu <Hakoyu@outlook.com>
2024-08-23 18:46:43 +0800

    为`ReactiveObjectX`添加`InitializeBefore`和`InitializeAfter`, 用于在初始化前后执行事件

--- HKW.ReactiveUI/ReactiveObjectX.cs
+++ HKW.ReactiveUI/ReactiveObjectX.cs
@@ -8,11 +8,23 @@ public partial class ReactiveObjectX : ReactiveObject
     /// <inheritdoc/>
     public ReactiveObjectX()
     {
+        InitializeBefore();
         InitializeReactiveObject();
+        InitializeAfter();
     }
+
+    /// <summary>
+    /// 初始化之前
+    /// </summary>
+    protected virtual void InitializeBefore() { }
 
     /// <summary>
     /// 初始化 (用于自动生成)
     /// </summary>
     protected virtual void InitializeReactiveObject() { }
+
+    /// <summary>
+    /// 初始化之后
+    /// </summary>
+    protected virtual void InitializeAfter() { }
 }
